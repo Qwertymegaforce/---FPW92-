8850408bcc8fd85d1397847c627190532de14a12
Qwertymegaforce <115457802+Qwertymegaforce@users.noreply.github.com>
2023-02-20 12:24:29 +0300

    Delete Спасибо за гайды directory

--- Спасибо за гайды/ads/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from ads.models import *
-
-
-admin.site.register(Category)
--- Спасибо за гайды/ads/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import *
-
-
-class CreateAdForm(forms.ModelForm):
-    class Meta:
-        model = Ads
-        fields = ['title', 'category', 'text', 'file']
-
